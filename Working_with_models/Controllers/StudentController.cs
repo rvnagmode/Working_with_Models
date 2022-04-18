0daa5d1db099471dd9c4db549789b405030c10b7
rvnagmode <101249215+rvnagmode@users.noreply.github.com>
2022-04-18 13:02:37 +0530

    Add project files.

---
+++ Working_with_models/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Working_with_models.Models;
+
+namespace Working_with_models.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Students stud)
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(Students stud)
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(Students stud)
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult List(Students stud)
+        {
+            return View();
+        }
+    }
+}
---
+++ Working_with_models/Models/Students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity; 
+namespace Working_with_models.Models
+ 
+{
+    public class Students
+    {
+        public int rollno { get; set; }
+        public string name { get; set; }
+        public int percentage { get; set; }
+
+    }
+    public class ProductContext : DbContext
+    {
+        
+        public DbSet<Students> student { get; set; }
+    }
+
+}
